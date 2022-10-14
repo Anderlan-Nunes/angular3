79d3b1501b3d5e1aa61f7ecf465d98807a3add05
loja <loja5@gmail.com>
2022-10-14 20:08:13 -0300

    organizando as pastas do projeto

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgifComponent } from './ngif/ngif.component';
 import { NgforComponent } from './ngfor/ngfor.component';
 import { NgclassComponent } from './ngclass/ngclass.component';
 import { NgstyleComponent } from './ngstyle/ngstyle.component';
-import { FormsModule } from '@angular/forms';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightDirective } from './shared/highlight.directive';
+import { DiretivaCustomizadaAtributoComponent } from './diretiva-customizada-atributo/diretiva-customizada-atributo.component';
+import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,8 @@ import { HighlightDirective } from './shared/highlight.directive';
     NgstyleComponent,
     OperadorElvisComponent,
     HighlightMouseDirective,
+    FundoAmareloDirective,
+    DiretivaCustomizadaAtributoComponent,
     DiretivasCustomizadasComponent,
     HighlightDirective
   ],
